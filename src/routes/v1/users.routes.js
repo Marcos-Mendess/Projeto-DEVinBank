a44b347bce13ada0d91f6704f74b9b9711e6e00a
anao242 <marcos_fmendes@hotmail.com>
2022-01-30 18:22:53 -0300

    Fix bug
    Fixando bug do require userController

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const usersRoutes = express.Router();
-const userController = require("../../controllers/userController");
+const userController = require("../../controllers/UserController");
 
 usersRoutes.get("/v1/user/:id", userController.indexOne);
 usersRoutes.post("/v1/user", userController.create);
